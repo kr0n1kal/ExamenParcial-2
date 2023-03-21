8bb9bd58e161982e8386fffcd93f43dc000c365f
kr0n1kal <77345149+kr0n1kal@users.noreply.github.com>
2023-03-21 09:17:32 -0600

    Numeros pares en python
    Creación de archivo verificando que un numero sea par en Python

---
+++ par.py
@@ -0,0 +1,6 @@
+#VERIFICAR QUE UN NUMERO SEA PAR
+num = int(input("Ingrese un numero a verificar si es par: "))
+if num%2==0:
+    print("El numero ingresado es par!")
+else:
+    print("El numero ingresado no es par!")
